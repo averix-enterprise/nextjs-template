dd9776a015bfe15ab181e4cfe0b44fce4451d42e
ReaperMaga <reapermaga@gmail.com>
2023-09-06 05:18:20 +0200

    :rocket: Init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl text-gray-300">
+        Thank you for using this template
+      </h1>
+    </main>
+  )
+}
