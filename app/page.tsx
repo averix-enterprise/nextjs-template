994ce092944b95261f0b604f13e8e9db510d0e26
ReaperMaga <reapermaga@gmail.com>
2023-10-20 00:35:35 +0200

    Removed whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 
 export default function Home(): ReactElement {
   return (
-    <main className=" flex min-h-screen items-center justify-center bg-black">
+    <main className="flex min-h-screen items-center justify-center bg-black">
       <h1 className="text-4xl text-gray-300">
         Thank you for using this template
       </h1>
